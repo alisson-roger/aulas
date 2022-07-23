455f96b55993a7bb649f720dd8693a481351cff6
Roger! <arogerlima@gmail.com>
2022-07-23 20:03:08 -0300

    Adicionando Navegação

---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,16 @@
+import { useNavigate } from "react-router-dom"
+
+export const Login = () => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/pagina-inicial')
+    }
+    
+    return(
+        <div>
+            Login
+            <button onClick={handleClick}>Página inicial</button>
+        </div>
+    )
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
 import React from "react"
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
-import { Dashboard } from "../pages"
+import { Dashboard, Login } from "../pages"
 
 export const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/pagina-inicial' element={<Dashboard/>}/>
+                <Route path='/entrar' element={<Login/>}/>
                 
                 <Route path='/*' element={<Navigate to="/pagina-inicial" />}/>
             </Routes>
